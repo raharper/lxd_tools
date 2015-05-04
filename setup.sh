c9202d2923b9d6d93267443dd06c77c49f81ecdd
Ryan Harper <ryan.harper@canonical.com>
2015-05-04 08:00:31 -0500

    Fixes for ppc64le

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash -x
+case `uname -m` in
+  ppc64le)  ARCH='ppc64el';;
+  *) ARCH=`uname -m`;;
+esac
+RELEASE=${1:-'vivid'}
 
 sudo apt-get install -y git socat parallel
 
 # get pylxd and apply my patch
 [ ! -d "pylxd" ] && {
-    git clone https://github.com/zulcss/pylxd
+    https_proxy=$https_proxy git clone https://github.com/zulcss/pylxd
     (cd pylxd && patch -p1 < ../pylxd_rharper.patch)
 }
 
@@ -14,17 +19,17 @@ if ! screen -ls socat | grep -q socat; then
 fi
 
 # import trusty image
-if ! lxc image list | grep -q ubuntu-trusty; then
-    lxd-images import lxc ubuntu trusty amd64 --alias ubuntu-trusty
+if ! lxc image list | grep -q ubuntu-vivid; then
+    https_proxy=$https_proxy lxd-images import lxc ubuntu $RELEASE $ARCH --alias ubuntu-$RELEASE
 fi
 
 # launch and stop it; we want the instantiated rootfs
-if ! lxc list | grep -q trusty-template; then
-   lxc launch ubuntu-trusty trusty-template
-   lxc stop --force trusty-template 
+if ! lxc list | grep -q $RELEASE-template; then
+   lxc launch ubuntu-$RELEASE $RELEASE-template
+   lxc stop --force $RELEASE-template 
 fi
 
 echo 
 echo "Now run:"
-echo "  sudo ./create_and_delete_pylxd test1 trusty-template/rootfs"
+echo "  sudo ./create_and_delete_pylxd test1 $RELEASE-template/rootfs"
 echo 
--- walk.sh
+++ walk.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
+START=${1-0}
+END=${2-1000}
+PAR=${3-20}
+LOGDIR=/tmp
 
 # stop a free < $MIN_FREE 
 MIN_FREE=${1:-"524288"}  # 512MB
 
+spawn() {
+   set -e
+   name=${1}
+   date +%s;
+   FREE=$(free | awk '/Mem:/ {print $4}')
+   [ ${FREE} -lt ${MIN_FREE} ] && { echo "Low Ram, exiting"; return 1; }
+   sudo ./create_pylxd_overlay $name vivid-template/rootfs
+   date +%s;
+}
+
+
 date +%s;
-for v in $(seq -w 0 1000); do
-    sudo ./create_pylxd_overlay $v || break
-    date +%s;
-    FREE=$(free | awk '/Mem:/ {print $4}')
-    [ ${FREE} -lt ${MIN_FREE} ] && { echo "Low Ram, exiting"; break; }
+for v in $(seq -w $START $END); do
+    PIDS=""
+    for i in $(seq $PAR); do
+        spawn $v 2>&1 | tee $LOGDIR/lxc-${v}.out &
+        PIDS="$PIDS $!"
+    done
+    for pid in $PIDS; do
+        wait $pid
+    done
 done;
 date +%s
